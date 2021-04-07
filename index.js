f098bb7698099dbfdbb4fb6709ad15080889a4fd
lisellelogan <liselle.logan@yahoo.com>
2021-04-06 21:39:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+
+//multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+//random
+const random = Math.floor(Math.random() * 1000) + 1;
+
+//mod
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4
+
+//max
+const max = Math.max(5, 10, 15, 20)
+
